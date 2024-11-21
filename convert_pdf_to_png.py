e16773e5fb81ce82afc84e066ae9f81ab1266337
AdrianM0 <adrianmirza81@gmail.com>
2024-11-21 11:09:24 +0100

    update hard mode

--- convert_pdf_to_png.py
+++ convert_pdf_to_png.py
@@ -2,11 +2,11 @@ from pathlib import Path
 from pdf2image import convert_from_path
 
 # Path to the PDF file
-pdf_files = Path(".").rglob("MS*.pdf")
-
+pdf_files = Path(".").rglob("hard/MS*.pdf")
+# name is MS_1_2
 for pdf_file in pdf_files:
-    index = str(pdf_file).split(".pdf")[0]
+    index = str(pdf_file).split("/")[-1].split(".")[0].split("_")[-1]
     pages = convert_from_path(pdf_file, 500)
     for i, page in enumerate(pages):
-        page.save(f"easy_{index}.png", "PNG")
+        page.save(f"hard_{index}.png", "PNG")
 
